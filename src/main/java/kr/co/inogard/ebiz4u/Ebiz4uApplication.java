3f7f33ed2e9cabd4d5ecae023a87f35cc5d0b78a
이노가드_김원기 <metlin@inogard.co.kr>
2020-07-27 18:10:06 +0900

    2020.07.27 첫 번째 올림.

---
+++ src/main/java/kr/co/inogard/ebiz4u/Ebiz4uApplication.java
@@ -0,0 +1,13 @@
+package kr.co.inogard.ebiz4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ebiz4uApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ebiz4uApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/inogard/ebiz4u/Ebiz4uApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.inogard.ebiz4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ebiz4uApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
